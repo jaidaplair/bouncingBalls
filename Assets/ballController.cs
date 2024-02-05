ee6fee4f717928955913dcf472058b65fd8a8895
Jaida Plair <jaiplair@gmail.com>
2024-02-05 09:40:19 -0500

    ball sound, added ball

--- Assets/ballController.cs
+++ Assets/ballController.cs
@@ -12,11 +12,28 @@ public class ballController : MonoBehaviour
 
         //set velocity
         rb.velocity = new Vector2(Random.Range(-200f, 200f), Random.Range(-200f,200));
+
+        AudioSource audioSource;//reference to audio source
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) //create popping noise whenever person presses space bar
+        {
+            AudioSource audioSource;//reference to audio source
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Debug.Log("Ouch! That hurt man!");
+        AudioSource audioSource;//reference to audio source
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+
     }
 }
